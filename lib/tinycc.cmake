aae352df8bd7f23719995ec7cbb5929b54f524be
MioYiSama <mioyisama@gmail.com>
2025-08-17 07:20:03 +0800

    update

--- lib/tinycc.cmake
+++ lib/tinycc.cmake
@@ -1,15 +1,11 @@
-set(TINYCC_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/tinycc)
+set(TINYCC_SOURCE_DIR ${CMAKE_SOURCE_DIR}/lib/tinycc)
 set(TINYCC_LIBRARY libtcc.a)
 
-execute_process(
-        COMMAND ./configure --prefix=${CMAKE_SOURCE_DIR}/build/install
-        WORKING_DIRECTORY ${TINYCC_SOURCE_DIR}
-)
-execute_process(
-        COMMAND make -j12
-        WORKING_DIRECTORY ${TINYCC_SOURCE_DIR}
-)
-execute_process(
-        COMMAND make install
-        WORKING_DIRECTORY ${TINYCC_SOURCE_DIR}
-)
+if (NOT EXISTS ${INSTALL_DIR}/lib/${TINYCC_LIBRARY})
+    execute_process(
+            COMMAND ./configure --prefix=${INSTALL_DIR}
+            COMMAND make -j12
+            COMMAND make install
+            WORKING_DIRECTORY ${TINYCC_SOURCE_DIR}
+    )
+endif ()
